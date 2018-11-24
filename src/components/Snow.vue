7831dc88454ab8ed719dfc92a47c146f63d591e6
kazukinakamuraKN <kajirikajiri@gmail.com>
2018-11-24 09:32:18 +0900

    refactor

--- src/components/Snow.vue
+++ src/components/Snow.vue
@@ -1,7 +1,7 @@
 <template>
   <section>
     <div class="snow">
-      <Snowf v-bind:color="sendColor" :amount="50" :size="3" :speed="0.5" :wind="0" :opacity="0.8" :swing="1" :image="null" :zIndex="null" :resize="true"/>
+      <Snowf v-bind:color="send_color_to_snow" :amount="50" :size="3" :speed="0.5" :wind="0" :opacity="0.8" :swing="1" :image="null" :zIndex="null" :resize="true"/>
     </div>
   </section>
 </template>
@@ -11,7 +11,7 @@
 import Snowf from 'vue-snowf'
 
 export default {
-  props: ['sendColor'],
+  props: ['send_color_to_snow'],
   data() {
     return {
       nowColor: '#ffffff'
